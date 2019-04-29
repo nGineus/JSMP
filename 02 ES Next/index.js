8841c0727ee856b1eb73c83a9b931fd00854a19a
ngineus <ngineus@gmail.com>
2019-04-29 13:47:53 +0300

    Init

--- 02 ES Next/index.js
+++ 02 ES Next/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// • Promisify XMLHTTPRequest inside a class with methods: get, post, put, delete.
+// Methods might be used by reference. For example: `[‘http://google.com’].map(Http.get)`.
+//
+// • Use async/await syntax.
+
+class ApiService {
+    request = () => {
+
+    }
+}
+
+let api = new ApiService()
