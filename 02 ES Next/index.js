86b5a5c965fdbba43b8ec57b2544a375f6dc7c0d
nGineus <ngineus@gmail.com>
2019-05-07 18:40:35 +0300

    [03] JS.Next Init

--- 02 ES Next/index.js
+++ 02 ES Next/index.js
@@ -1,9 +1,24 @@
 'use strict';
 
-// • Promisify XMLHTTPRequest inside a class with methods: get, post, put, delete.
-// Methods might be used by reference. For example: `[‘http://google.com’].map(Http.get)`.
-//
-// • Use async/await syntax.
+// Part 1
+//  Promisify XMLHTTPRequest inside a class with methods: get, post, put, delete. Methods might
+// be used by reference. For example: `[‘http://google.com’].map(Http.get)`.
+//  Use async/await syntax.
+//  Play around with, for example SWAPI, to make couple of nested requests (request based on
+// previous request).
+
+// Part 2
+//  Create custom class `Book` with constructor param `character` with fields: `name`, `age`.
+//  Inside `Book` instance interpolate `name` and `age` into string template (feel free to create any
+// story you like).
+//  Create iterator property to be able to read your cool story line by line inside for..of loop.
+//  Export `Book` as module. Do not create instance in the same file.
+//  Use as many ES.Next features as you can.
+
+//   Part 3
+//  Create custom class `Bookshelf` and add `Book` instances to it.
+//  Implement method `read` as a generator that will return iterator over all added books. Read all
+// books inside this loop.
 
 const ENV = {
     HOST: 'https://swapi.co/api/'
